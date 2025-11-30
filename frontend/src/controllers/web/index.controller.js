1898b42a874d89a8c411e2a0a33081b4ad67dc3a
MarkKim-Humber <n01716085@humber.ca>
2025-11-30 16:19:57 -0500

    added home page

--- frontend/src/controllers/web/index.controller.js
+++ frontend/src/controllers/web/index.controller.js
@@ -1,5 +1,15 @@
+// function home(req, res) {
+//   const { flash } = req.query;
+//   const url = flash ? `/movies?flash=${encodeURIComponent(flash)}` : '/movies';
+//   return res.redirect(url);
+// }
+
+// export {
+//   home
+// };
+
 function home(req, res) {
-  return res.redirect('/movies');
+  return res.render('home/home', {});
 }
 
 export {
